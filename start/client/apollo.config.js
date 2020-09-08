328315fdce511694ef21ee99fa2b372b5ba671d8
Yadab Sutradhar <yadabsd@invarianttele.com>
2020-09-08 16:39:11 +0600

    first commit

---
+++ start/client/apollo.config.js
@@ -0,0 +1,16 @@
+// // You must be using Apollo Server v2.15.0+ to use this option.
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+//   engine: {
+//     reportSchema: true,
+//     variant: "current"
+//   }
+// });
+
+module.exports = {
+  client: {
+    name: 'Space Explorer [web]',
+    service: 'test-my-graph-for-practice',
+  },
+};
